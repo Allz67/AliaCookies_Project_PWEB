1336dd2a5f67f62bcf6e1a7f118470f917878e04
Allz67 <thaliasalsa76@mail.com>
2026-06-13 07:15:16 +0700

    fix migration deskripsi

--- tugas-laravel/database/migrations/2026_05_20_211535_add_deskripsi_to_products_table.php
+++ tugas-laravel/database/migrations/2026_05_20_211535_add_deskripsi_to_products_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->text('deskripsi')->default('Deskripsi belum tersedia.')->after('harga');
+            $table->text('deskripsi')->nullable()->after('harga');
         });
     }
 
